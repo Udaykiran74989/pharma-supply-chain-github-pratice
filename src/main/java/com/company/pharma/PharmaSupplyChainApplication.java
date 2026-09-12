165ed38f8bdae61e766df905c3898b14c7a2a90b
shankar <shankarmhetre759@gmail.com>
2026-09-12 12:06:06 +0530

    Updated pharma supply chain application

--- src/main/java/com/company/pharma/PharmaSupplyChainApplication.java
+++ src/main/java/com/company/pharma/PharmaSupplyChainApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PharmaSupplyChainApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PharmaSupplyChainApplication.class, args);
+        System.out.println("Hello Shankar Mhetre");
+        System.out.println("Hello Shankar Mhetre");
     }
+
+
 }
